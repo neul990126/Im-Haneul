edf0e8ec94b78ff9b97c2f68a90afc630a84157a
Im Haneul <neury0126@gmail.com>
2021-06-15 18:58:37 +0900

    no message

---
+++ solution.ipynb.py
@@ -0,0 +1,20 @@
+class Point:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+
+    def show():
+        print("(",x,",",y,")")
+
+a=Point(1,2)
+a.show()
+
+b=Point(3,4)
+b.show()
+
+a=a+b
+a.show()
+
+
+
+
